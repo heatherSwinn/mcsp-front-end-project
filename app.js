a9394e6538ad7a992d24d216c24d96802e9bcf63
Heather Swinn <h.swinn@Heathers-MacBook-Air.local>
2023-11-29 15:58:26 -0600

    features work

--- app.js
+++ app.js
@@ -1,34 +1,75 @@
-const apiKey = 'live_ernsSQkla2IjMgahv077u27ytmdElqoYsznq2xwLylnz14pQhiaLbjhWqcd1zhQ1';
+//wrap entire code in DOMContentLoaded event listener to run code after DOM is fully loaded
+// document.addEventListener("DOMContentLoaded", function() {
+//     //declare dog API and key
+//     let searchBar = document.getElementById("search");
+//     let submitButton = document.querySelector("#submit");
+//     let resultContainer = document.getElementById("result-container");
+                            
+    // submitButton.addEventListener("click", function() {
+    //     // Get the value from the search bar
+    //     let searchTerm = searchBar.value;
 
-function fetchData(breedName) {
-    const apiUrl = `https://api.thedogapi.com/v1/breeds/${breedName}`;
+        // Make the API request using the entered search term
+        // });
+    // })
+        // Function to display the API results
+        // function displayResults(data) {
+        //     // Clear previous results
+        //     resultContainer.empty();
+        //     e.preventDefault();
+        //     // Display each result in a new div
+        //     data.forEach(breed => {
+        //         var newDiv = document.createElement("div");
+        //         newDiv.textContent = breed.name;
+        //         resultContainer.appendChild(newDiv);
+        //     });
+        // }
+    // });
 
-    fetch(`${apiUrl}?api_key=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-        // Handle the retrieved data
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
-}
-
-// Add an input field for the user to enter the breed name
-const inputElement = document.createElement('input');
-inputElement.type = 'text';
-inputElement.placeholder = 'Enter breed name';
-document.body.appendChild(inputElement);
-
-// Add a button to trigger the API call
-const buttonElement = document.createElement('button');
-buttonElement.textContent = 'Get Breed Information';
-buttonElement.addEventListener('click', function() {
-    const breedName = inputElement.value.trim();
-    if (breedName) {
-        fetchData(breedName);
-    } else {
-        console.error('Please enter a valid breed name.');
+//*****START FROM SCRATCH WITH API WITHOUT A KEY. WE LOVE APIs */
+document.addEventListener("DOMContentLoaded", function() {
+    
+    //declare variables for API, button, and result container
+    let submitButton = document.getElementById("submit");
+    // console.log(submitButton);
+    let resultContainer = document.getElementById("result-container");
+    let apiUrl = 'https://dog-api.kinduff.com/api/facts';
+    let apiUrl2 = 'https://dog.ceo/api/breeds/image/random';
+    
+    
+    //function to display data
+    function displayResults(fact, img){
+        //clear previous results
+        // resultContainer.empty();
+        
+        //display result in new div
+        let factDiv = document.createElement("div");
+        factDiv.textContent = fact;
+        resultContainer.appendChild(factDiv);
+        
+        let dogImg = document.createElement("img")
+        dogImg.setAttribute("src", img);
+        resultContainer.appendChild(dogImg)
     }
+    
+    // $.get(apiUrl, (data) => {
+    //         console.log(data.facts[0]);
+    //     })
+        
+    // $.get(apiUrl2, (img) => {
+    //         console.log(img.message);
+    //     })
+        // Event listener for submit button
+        submitButton.addEventListener("click", function() {
+            event.preventDefault();
+            // Fetch data from both APIs
+            $.get(apiUrl, (data) => {
+
+                console.log('data');
+                $.get(apiUrl2, (img) => {
+                    displayResults(data.facts[0], img.message);
+                })
+            })
+            // Display the results
+        });
 });
-document.body.appendChild(buttonElement);
